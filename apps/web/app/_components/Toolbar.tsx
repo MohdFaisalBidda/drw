531bcbc5edd8094735fbc102efd128a5825759b6
MohdFaisalBidda <biddafaisal@gmail.com>
2025-02-16 16:37:40 +0530

    Revert "Feat: Highlight work."
    This reverts commit 86a708808b0826c2cc56f1091f22681da238a6d3.

--- apps/web/app/_components/Toolbar.tsx
+++ apps/web/app/_components/Toolbar.tsx
@@ -8,7 +8,6 @@ import {
   ArrowRight,
   Diamond,
   Eraser,
-  SquareActivity,
 } from "lucide-react";
 import { Tool } from "../../lib/draw";
 
@@ -28,7 +27,6 @@ function Toolbar({
     { type: "arrow", icon: <ArrowRight className="h-5 w-5" /> },
     { type: "diamond", icon: <Diamond className="h-5 w-5" /> },
     { type: "eraser", icon: <Eraser className="h-5 w-5" /> },
-    { type: "select", icon: <SquareActivity className="h-5 w-5" /> },
   ];
 
   return (
