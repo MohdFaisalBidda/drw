8db1a733a4a2940e80bcc67aca3da0703a4b2178
MohdFaisalBidda <biddafaisal@gmail.com>
2025-05-28 00:44:41 +0530

    o_o

--- apps/web/lib/authenticate.ts
+++ apps/web/lib/authenticate.ts
@@ -1,9 +1,8 @@
+import { authOptions } from "@/utils/auth";
 import { getServerSession } from "next-auth";
-import { getSession } from "next-auth/react";
-import { NextResponse } from "next/server";
 
 export default async function authenticate() {
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
     console.log(session,"session in authenticate");
     
     if (!session?.user) {
--- apps/web/utils/auth.ts
+++ apps/web/utils/auth.ts
@@ -82,6 +82,6 @@ export const authOptions: NextAuthOptions = {
             return session
         },
     },
-    secret: process.env.JWT_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
     debug: process.env.NODE_ENV === 'development'
 }
